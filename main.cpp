125e2c1e17684b1d74884a7640cf42c2cb6868b4
Fedor Kuyanov <feodor.kuyanov@gmail.com>
2023-04-02 01:49:13 +0300

    pytorch is cool

--- main.cpp
+++ main.cpp
@@ -1,7 +1,145 @@
+#include <algorithm>
+#include <ctime>
 #include <iostream>
+#include <map>
+#include <tuple>
+#include <vector>
+#include <random>
 
 using namespace std;
+using vec = vector<int>;
+using mat = vector<vec>;
+using tensor = vector<mat>;
+
+const int N = 2;
+const int M = 2;
+const int K = 2;
+
+mt19937 rnd(clock());
+
+tensor Zeros(size_t n, size_t m, size_t k) {
+    return tensor(n, mat(m, vec(k)));
+}
+
+vec Rand(size_t n) {
+    vec v(n);
+    for (size_t i = 0; i < n; i++) {
+        v[i] = (int)rnd() % 2;
+    }
+    return v;
+}
+
+vec Vectorize(const tensor &ts) {
+    vec res;
+    res.reserve(ts.size() * ts[0].size() * ts[0][0].size());
+    for (const mat &mat1 : ts) {
+        for (const vec &vec1 : mat1) {
+            for (int elem : vec1) {
+                res.push_back(elem);
+            }
+        }
+    }
+    return res;
+}
+
+vec operator-(const vec &v1, const vec &v2) {
+    vec v(v1.size());
+    for (size_t i = 0; i < v.size(); i++) {
+        v[i] = v1[i] ^ v2[i];
+    }
+    return v;
+}
+
+int Sum(const vec &v) {
+    int sum = 0;
+    for (int elem : v) {
+        sum += elem;
+    }
+    return sum;
+}
+
+tensor OuterProduct(const vec &v1, const vec &v2, const vec &v3) {
+    auto res = Zeros(v1.size(), v2.size(), v3.size());
+    for (size_t i = 0; i < v1.size(); i++) {
+        for (size_t j = 0; j < v2.size(); j++) {
+            for (size_t t = 0; t < v3.size(); t++) {
+                res[i][j][t] = v1[i] * v2[j] * v3[t];
+            }
+        }
+    }
+    return res;
+}
+
+void PrintTensor(const tensor &ts) {
+    for (const mat &mat1 : ts) {
+        for (const vec &vec1 : mat1) {
+            for (int elem : vec1) {
+                cout << elem;
+            }
+            cout << ' ';
+        }
+        cout << endl;
+    }
+}
+
+tensor GenerateTensor(size_t n, size_t k, size_t m) { // (n x k) * (k x m) -> (n x m)
+    tensor res = Zeros(n * k, k * m, n * m);
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            for (size_t t = 0; t < k; t++) {
+                res[i * k + t][t * m + j][i * m + j] = 1;
+            }
+        }
+    }
+    return res;
+}
+
+int Weight(const vec &vectorized) {
+    int sum = 0;
+    for (int elem : vectorized) {
+        sum += abs(elem);
+    }
+    return sum;
+}
 
 int main() {
-    cout << "Hello world!" << endl;
+    auto target = Vectorize(GenerateTensor(N, K, M));
+    auto zeros = Vectorize(Zeros(N * K, K * M, N * M));
+    const int mem_size = 1000;
+    const int mult_factor = 1 << (N * K + K * M + N * M);
+    vector<vec> layer = {target};
+    int cnt_layers = Sum(target);
+    for (int layer_id = 0; layer_id < cnt_layers; layer_id++) {
+//        cout << "layer " << layer_id << endl;
+//        for (const auto &v : layer) {
+//            for (int elem : v) cout << elem;
+//            cout << endl;
+//        }
+//        cout << endl;
+        vector<vec> next_layer;
+        for (const auto &v : layer) {
+            for (size_t t = 0; t < mult_factor; t++) {
+                vec v1(N * K), v2(K * M), v3(N * M);
+                for (int bit = 0; bit < N * K + K * M + N * M; bit++) {
+                    if (bit < N * K) v1[bit] = (t >> bit) & 1;
+                    else if (bit < N * K + K * M) v2[bit - N * K] = (t >> bit) & 1;
+                    else v3[bit - N * K - K * M] = (t >> bit) & 1;
+                }
+                auto prod = OuterProduct(v1, v2, v3);
+                next_layer.push_back(v - Vectorize(prod));
+            }
+        }
+        sort(next_layer.begin(), next_layer.end());
+        next_layer.resize(unique(next_layer.begin(), next_layer.end()) - next_layer.begin());
+        if (next_layer.size() > mem_size) {
+            nth_element(next_layer.begin(), next_layer.begin() + mem_size, next_layer.end(), [&](const auto &elem1, const auto &elem2) {
+                return Weight(elem1) < Weight(elem2);
+            });
+            next_layer.resize(mem_size);
+        }
+        layer = next_layer;
+        if (find(layer.begin(), layer.end(), zeros) != layer.end()) {
+            cout << layer_id + 1 << endl;
+        }
+    }
 }
---
+++ matmul.py
@@ -0,0 +1,114 @@
+import sys
+import torch
+import numpy as np
+from torch import nn
+from torch.utils.data import TensorDataset, DataLoader
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def generate_matrices(n, n_samples):
+    a = torch.normal(mean=0., std=5., size=(n_samples, n, n))
+    b = torch.normal(mean=0., std=5., size=(n_samples, n, n))
+    y = a @ b
+    a = a.reshape((n_samples, -1))
+    b = b.reshape((n_samples, -1))
+    x = torch.cat((a, b), dim=1)
+    return x, y
+
+
+class MatmulModel(nn.Module):
+    def __init__(self, n, k):
+        super().__init__()
+        self.layer1 = nn.Linear(2 * n ** 2, 2 * k, bias=False)
+        self.layer3 = nn.Linear(k, n ** 2, bias=False)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = x[:, :k] * x[:, k:]
+        x = self.layer3(x)
+        return x
+
+
+def train_and_test(model, optimizer, criterion, train_loader, test_loader, num_epochs, tol, alpha):
+    train_losses, test_losses = [], []
+    for epoch in range(1, num_epochs + 1):
+        model.train()
+        train_loss = 0
+        for X_batch, Y_batch in train_loader:
+            X_batch, Y_batch = X_batch.to(device), Y_batch.to(device)
+            preds = model(X_batch)
+            loss = criterion(preds, Y_batch)
+            loss_old = loss.item()
+            for param in model.parameters():
+                loss += alpha * torch.sum((param - torch.round(param)) ** 2)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            with torch.no_grad():
+                train_loss += loss_old * len(X_batch)
+        train_losses.append(train_loss / len(train_loader.dataset))
+
+        model.eval()
+        test_loss = 0
+        for X_batch, Y_batch in test_loader:
+            X_batch, Y_batch = X_batch.to(device), Y_batch.to(device)
+            with torch.no_grad():
+                preds = model(X_batch)
+                loss = criterion(preds, Y_batch)
+                test_loss += loss.item() * len(X_batch)
+        test_losses.append(test_loss / len(test_loader.dataset))
+        if test_losses[-1] < tol:
+            break
+    return train_losses, test_losses
+
+
+def plot_losses(train_losses, test_losses):
+    plt.yscale('log')
+    plt.plot(train_losses, label='Train')
+    plt.plot(test_losses, label='Test')
+    plt.legend()
+
+
+def output_algorithm(model):
+    with torch.no_grad():
+        C1, C2 = [param for param in model.parameters()]
+        for t in range(2 * k):
+            print(f'y_{t} = ', end='')
+            for ind in range(n ** 2):
+                print(f'{round(C1[t][ind].item(), 3)} * A_{ind // n}{ind % n} + ', end='')
+            for ind in range(n ** 2):
+                print(f'{round(C1[t][n ** 2 + ind].item(), 3)} * B_{ind // n}{ind % n} + ', end='')
+            print('0')
+        for t in range(k):
+            print(f'z_{t} = y_{t} * y_{t + k}')
+        for ind in range(n ** 2):
+            print(f'C_{ind // n}{ind % n} = ', end='')
+            for t in range(k):
+                print(f'{round(C2[ind][t].item(), 3)} * z_{t} + ', end='')
+            print('0')
+
+
+n = int(sys.argv[1])
+X, Y = generate_matrices(n, 1024)
+X = X.reshape((len(X), -1)).float()
+Y = Y.reshape((len(Y), -1)).float()
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+k = int(sys.argv[2])
+tol = 1e-4
+for t in range(1, 101):
+    model = MatmulModel(n, k).to(device)
+    optimizer = torch.optim.ASGD(model.parameters(), lr=1e-4)
+    train_loader = DataLoader(TensorDataset(X_train, Y_train), batch_size=128, shuffle=True)
+    test_loader = DataLoader(TensorDataset(X_test, Y_test), batch_size=128, shuffle=False)
+    train_losses, test_losses = train_and_test(model, optimizer, nn.MSELoss(), train_loader, test_loader,
+                                               num_epochs=20000, tol=tol, alpha=1e-4)
+    print(f'Attempt {t}: train_loss = {train_losses[-1]}, test_loss = {test_losses[-1]}')
+    if test_losses[-1] < tol:
+        output_algorithm(model)
+        break
+
+# plot_losses(train_losses, test_losses)
